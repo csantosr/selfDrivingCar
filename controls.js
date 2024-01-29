8da3a80ce02f6d19b6c04c2c8d4650c62610d479
Cristian Santos <csantosr@unal.edu.co>
2024-01-29 18:37:04 -0500

    Self driving

---
+++ controls.js
@@ -0,0 +1,53 @@
+class Controls {
+  constructor(type) {
+    this.forwards = false;
+    this.left = false;
+    this.right = false;
+    this.reverse = false;
+
+    switch(type) {
+      case "KEYS":
+        this.#addKeyboardListeners();
+        break;
+      case "DUMMY":
+        this.forwards = true;
+        break;
+    }
+  }
+
+  #addKeyboardListeners() {
+    document.onkeydown= (event) => {
+      switch(event.key) {
+        case 'ArrowLeft':
+          this.left = true;
+          break;
+        case 'ArrowRight':
+          this.right = true;
+          break;
+        case 'ArrowUp':
+          this.forwards = true;
+          break;
+        case 'ArrowDown':
+          this.reverse = true;
+          break;
+      }
+    }
+
+    document.onkeyup= (event) => {
+      switch(event.key) {
+        case 'ArrowLeft':
+          this.left = false;
+          break;
+        case 'ArrowRight':
+          this.right = false;
+          break;
+        case 'ArrowUp':
+          this.forwards = false;
+          break;
+        case 'ArrowDown':
+          this.reverse = false;
+          break;
+      }
+    }
+  }
+}
